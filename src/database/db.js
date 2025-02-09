8c752fb26a6cc0219aa6365d7b25654e4615b0bd
Rishant Rana <kiturana378@gmail.com>
2025-02-09 16:46:57 +0530

    database connection made

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const connectDB=async()=>{
+   try {
+     const connect=await mongoose.connect(`${process.env.MONGO_URL}/`);
+     if(connect){
+        console.log(`Connection to database established : ${connect.connection.host}`);
+     }
+     else throw new Error("Connection to database not established")
+   } catch (error) {
+        console.log(`Error connecting to database : ${error.message}`);
+        
+   }
+}
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import connectDB from './database/db.js';
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
 // middlewares
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
 // routes
 
 
 const port=process.env.PORT || 8001;
 
+// database connection and app starting
+connectDB();
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
