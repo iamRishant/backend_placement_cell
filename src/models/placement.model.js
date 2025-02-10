25f7629924c3d15caa1fa7e82641854fc844243b
Nektar-pi <piyushsdg@gmail.com>
2025-02-10 06:50:58 +0530

    -Placement Model

---
+++ src/models/placement.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const placementSchema = new mongoose.Schema({
+    companyName:{
+        type: String,
+        required: true,
+        lowercase: true
+    },
+
+    companyInfo:{
+        type: mongoose.Schema.Types.Mixed,
+        required: true,
+    },
+
+    date:{
+        type: Date,
+        default: Date.now,  
+        required: true,
+    },
+    jobTitle:{
+        type: String,
+        required: true,
+    },
+    
+    jobDescription: mongoose.Schema.Types.Mixed,
+
+    eligibility: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true,
+    },
+
+    payPackage: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true,
+    },
+
+    bond: {
+        type: Boolean,
+        required: true,
+    },  
+    
+    bondDescription: {
+        type: String,
+        required: function(){ return this.bond;}
+    },
+    
+    formDeadline:{
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+
+    registerationLink: {
+        type: String,
+        required: true,
+    },
+}, {timestamps: true})
+
+export const User = mongoose.model("Placement", placementSchema)
